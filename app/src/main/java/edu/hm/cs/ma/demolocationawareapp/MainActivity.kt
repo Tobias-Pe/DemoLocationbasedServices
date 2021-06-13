64f9209caa2dfbaefe084cc0a15f253fca185fa0
Tobias Peslalz <peslalz.tobias@hm.edu>
2021-06-13 13:20:35 +0200

    :fire: optimise imports

--- app/src/main/java/edu/hm/cs/ma/demolocationawareapp/MainActivity.kt
+++ app/src/main/java/edu/hm/cs/ma/demolocationawareapp/MainActivity.kt
@@ -1,17 +1,13 @@
 package edu.hm.cs.ma.demolocationawareapp
 
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.hm.cs.ma.demolocationawareapp.databinding.ActivityMainBinding
-import edu.hm.cs.ma.demolocationawareapp.ui.location.LocationFragment
-import edu.hm.cs.ma.demolocationawareapp.util.REQUEST_PERMISSIONS_REQUEST_CODE
 
 class MainActivity : AppCompatActivity() {
 
