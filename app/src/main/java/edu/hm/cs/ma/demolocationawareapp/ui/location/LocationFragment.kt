f37b9c4fefc88b37e2efad1fc0385fac30977804
Tobias Peslalz <peslalz.tobias@hm.edu>
2021-06-13 12:38:59 +0200

    :sparkles: settings check and locationProvider configuration

--- app/src/main/java/edu/hm/cs/ma/demolocationawareapp/ui/location/LocationFragment.kt
+++ app/src/main/java/edu/hm/cs/ma/demolocationawareapp/ui/location/LocationFragment.kt
@@ -1,8 +1,11 @@
 package edu.hm.cs.ma.demolocationawareapp.ui.location
 
 import android.Manifest
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -14,14 +17,18 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 import edu.hm.cs.ma.demolocationawareapp.databinding.FragmentLocationBinding
 
 class LocationFragment : Fragment() {
 
+    private lateinit var locationRequest: LocationRequest
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private var isLocationClientConfigured: Boolean = false
 
     private lateinit var locationViewModel: LocationViewModel
     private lateinit var binding: FragmentLocationBinding
@@ -46,11 +53,22 @@ class LocationFragment : Fragment() {
             handleButtonClicked()
         }
 
+        initPermissionLauncher()
+
+        initLocationProvider()
+
+        isLocationClientConfigured = false
+
+        return root
+    }
+
+    private fun initPermissionLauncher() {
         requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestMultiplePermissions()
             ) {
                 val isGranted: Boolean = it.values.all { isGranted -> isGranted == true }
+                // Check permission status after requesting from the user
                 if (isGranted) {
                     // Permission is granted. Continue the action or workflow in your
                     // app.
@@ -66,10 +84,45 @@ class LocationFragment : Fragment() {
                     locationViewModel.setText("This feature will be unavailable to you until the permission to access your smartphones location is allowed.")
                 }
             }
+    }
 
+    private fun initLocationProvider() {
+        // Settings for fusedLocationClient
+        locationRequest = LocationRequest.create().apply {
+            interval = 100
+            fastestInterval = 100
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        // Init the LocationProvider
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
-        return root
+        checkSettings()
+    }
+
+    private fun checkSettings() {
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(requireContext())
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(
+                        requireActivity(),
+                        exception.statusCode
+                    )
+                    Log.i("Settings", "startResolutionForResult")
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
     }
 
     private fun handleButtonClicked() {
@@ -88,12 +141,27 @@ class LocationFragment : Fragment() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            Log.i("Permission", "Permissions granted. Getting location...")
-            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                locationViewModel.setText("Lat: ${location.latitude}\nLon: ${location.longitude}\nAccuracy:${location.accuracy}")
-            }
-        }
 
+            if (!isLocationClientConfigured) {
+                fusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    object : LocationCallback() {
+                        override fun onLocationResult(locationResult: LocationResult) {
+                            super.onLocationResult(locationResult)
+                        }
+                    }, Looper.myLooper()
+                )
+                isLocationClientConfigured = true
+            }
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    locationViewModel.setText("Lat: ${location.latitude}\nLon: ${location.longitude}\nAccuracy:${location.accuracy}")
+                }
+            }
+
+        } else {
+            checkSettings()
+        }
     }
 
     private fun requestPermission() {
