a9642b76824d3570b97ad85445a5a8d22a240414
Tobias Peslalz <peslalz.tobias@hm.edu>
2021-06-14 10:30:58 +0200

    :bento: Update welcome text

--- app/src/main/java/edu/hm/cs/ma/demolocationawareapp/ui/location/LocationViewModel.kt
+++ app/src/main/java/edu/hm/cs/ma/demolocationawareapp/ui/location/LocationViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class LocationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Hello there!"
     }
     val text: LiveData<String> = _text
 
